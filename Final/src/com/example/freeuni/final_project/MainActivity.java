995bef17f7910fea5a08d2b3baac024ee078f6cc
salometsiramua <stsir11@freeuni.edu.ge>
2014-07-01 15:28:52 +0400

    road :))

--- Final/src/com/example/freeuni/final_project/MainActivity.java
+++ Final/src/com/example/freeuni/final_project/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.freeuni.final_project;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
+	RelativeLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = (RelativeLayout) findViewById(R.id.road);
+        layout.setBackgroundColor(Color.DKGRAY);
         System.out.println("elene");
     }
 
