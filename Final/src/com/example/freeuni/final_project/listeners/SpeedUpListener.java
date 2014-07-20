5460f7b3dffea753a06541a9af4d3b38ddf58250
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-07-20 14:25:57 +0400

    2 axali metodi

--- Final/src/com/example/freeuni/final_project/listeners/SpeedUpListener.java
+++ Final/src/com/example/freeuni/final_project/listeners/SpeedUpListener.java
@@ -3,4 +3,6 @@ package com.example.freeuni.final_project.listeners;
 public interface SpeedUpListener {
 
 	public void speedUpListner();
+	public void onSubmitFriendsId(int id);
+	public void onWaitForFriend();
 }
