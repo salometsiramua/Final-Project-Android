79ce1fe46e4216d01c551ee4e5de3dd8e47fda40
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-07-20 14:25:16 +0400

    servertan mesijebis gzavna da migeba

--- Final/src/com/example/freeuni/final_project/App.java
+++ Final/src/com/example/freeuni/final_project/App.java
@@ -12,65 +12,94 @@ import com.example.freeuni.final_project.listeners.SpeedUpListener;
 import android.R.integer;
 import android.app.Application;
 
-public class App extends Application implements SpeedUpListener{
+public class App extends Application implements SpeedUpListener {
 	private Socket socket;
 	public static final String HOST_NAME = "192.168.0.184";
-    public static final int PORT_NUMBER = 8095;
-    private static final String ID_STRING ="YourId:";
-    private int myId;
+	public static final int PORT_NUMBER = 8095;
+	private static final String ID_STRING = "YourId:";
+	private static final String CONNECTION_STRING = "connect:";
+	private static final String WANTS_CONNECTION_STRING = "wantsConnection:";
+	private static final String GAME_STARTED = "gameStarted:";
+	private int myId;
+	private int theirId;
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
-		
+
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				startConnection();
-			
-				
+				waitForGameToStart();
 			}
 
-			
 		}).start();
 	}
-	private void startConnection() {
-	        try {
-	           socket = new Socket(HOST_NAME, PORT_NUMBER);
-	           BufferedReader in = new BufferedReader(new InputStreamReader(
-						socket.getInputStream()));
-	           String inputLine;
-	           while ((inputLine = in.readLine()) != null) {
-	        	   if ((inputLine.length() > ID_STRING.length())&& inputLine.substring(0, ID_STRING.length()).equals(ID_STRING)) {
-	        	   
-	        		   String id = inputLine
-								.substring(ID_STRING.length());
-	        		   myId = Integer.parseInt(id);
-	        		   System.out.println("my id " + myId);
-	        		   break;
-	        	   }
-	        	   
-	        	   
-	           }
-       
-	        } catch (UnknownHostException e) {
-	            System.err.println("Don't know about host " + HOST_NAME);
-	            System.exit(1);
-	        } catch (IOException e) {
-	            System.err.println("Couldn't get I/O for the connection to " +
-	            		HOST_NAME);
-	            System.exit(1);
-	        }
+
+	protected void waitForGameToStart() {
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				if ((inputLine.length() > GAME_STARTED.length())
+						&& inputLine.substring(0, GAME_STARTED.length()).equals(
+								GAME_STARTED)) {
+
+					//start game
+					break;
+				}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
+
+	private void startConnection() {
+		try {
+			socket = new Socket(HOST_NAME, PORT_NUMBER);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				if ((inputLine.length() > ID_STRING.length())
+						&& inputLine.substring(0, ID_STRING.length()).equals(
+								ID_STRING)) {
+
+					String id = inputLine.substring(ID_STRING.length());
+					myId = Integer.parseInt(id);
+					System.out.println("my id " + myId);
+					break;
+				}
+
+			}
+
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host " + HOST_NAME);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to "
+					+ HOST_NAME);
+			System.exit(1);
+		}
+
+	}
+
 	@Override
 	public void speedUpListner() {
 
-	    PrintWriter out = null;
+		PrintWriter out = null;
 		try {
-			if(socket!=null){
-				
+			if (socket != null) {
+
 				out = new PrintWriter(socket.getOutputStream(), true);
 				out.println("speadup");
 			}
@@ -78,8 +107,63 @@ public class App extends Application implements SpeedUpListener{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
-		
-		
+
 	}
+
+	@Override
+	public void onSubmitFriendsId(int id) {
+
+		PrintWriter out = null;
+		try {
+			if (socket != null) {
+
+				out = new PrintWriter(socket.getOutputStream(), true);
+				out.println(CONNECTION_STRING + id);
+				theirId = id;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onWaitForFriend() {
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				BufferedReader in = null;
+				try {
+					in = new BufferedReader(new InputStreamReader(socket
+							.getInputStream()));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				String inputLine;
+				try {
+					while ((inputLine = in.readLine()) != null) {
+						if ((inputLine.length() > WANTS_CONNECTION_STRING.length())&& inputLine.substring(0,WANTS_CONNECTION_STRING.length()).equals(WANTS_CONNECTION_STRING)) {
+
+							String theirIdStr = inputLine.substring(WANTS_CONNECTION_STRING.length());
+							theirId = Integer.parseInt(theirIdStr);
+							break;
+						}
+
+					}
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+		}).start();
+	}
+
 }
