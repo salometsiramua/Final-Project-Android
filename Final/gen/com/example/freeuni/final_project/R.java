bfb5f545b438b401fd986ce7411a221b7c127baf
salometsiramua <stsir11@freeuni.edu.ge>
2014-07-21 14:40:00 +0400

    image changes

--- Final/gen/com/example/freeuni/final_project/R.java
+++ Final/gen/com/example/freeuni/final_project/R.java
@@ -23,34 +23,39 @@ public final class R {
     public static final class drawable {
         public static final int green_car=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int lighth_green_car=0x7f020002;
-        public static final int pedal=0x7f020003;
-        public static final int purple_car=0x7f020004;
-        public static final int race_flag=0x7f020005;
-        public static final int red_car=0x7f020006;
-        public static final int start=0x7f020007;
-        public static final int yellow_car=0x7f020008;
+        public static final int icon=0x7f020002;
+        public static final int lighth_green_car=0x7f020003;
+        public static final int pedal=0x7f020004;
+        public static final int purple_car=0x7f020005;
+        public static final int race_flag=0x7f020006;
+        public static final int red_car=0x7f020007;
+        public static final int start=0x7f020008;
+        public static final int yellow_car=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int choose_scroll=0x7f08000a;
-        public static final int join_friend=0x7f08000f;
+        public static final int action_settings=0x7f080016;
+        public static final int imageView1=0x7f08000a;
+        public static final int imageView2=0x7f08000b;
+        public static final int imageView3=0x7f08000c;
+        public static final int imageView4=0x7f08000d;
+        public static final int imageView5=0x7f08000e;
+        public static final int join_friend=0x7f080013;
         public static final int leftLine=0x7f080001;
         public static final int left_wheel=0x7f080008;
         public static final int line=0x7f080006;
         public static final int my_car=0x7f080004;
-        public static final int one_player=0x7f08000b;
+        public static final int one_player=0x7f08000f;
         public static final int rightLine=0x7f080003;
         public static final int right_wheel=0x7f080009;
         public static final int road=0x7f080000;
         public static final int speedometer=0x7f080002;
-        public static final int startLayout=0x7f08000d;
-        public static final int start_new_game=0x7f08000e;
+        public static final int startLayout=0x7f080011;
+        public static final int start_new_game=0x7f080012;
         public static final int steering_wheel=0x7f080007;
-        public static final int submit=0x7f080011;
+        public static final int submit=0x7f080015;
         public static final int their_car=0x7f080005;
-        public static final int two_players=0x7f08000c;
-        public static final int write_friends_id=0x7f080010;
+        public static final int two_players=0x7f080010;
+        public static final int write_friends_id=0x7f080014;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
