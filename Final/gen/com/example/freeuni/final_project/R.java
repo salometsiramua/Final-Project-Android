b213e27916bc1e491449b6b8c5f64dd17f2f4eba
salometsiramua <stsir11@freeuni.edu.ge>
2014-07-21 16:42:51 +0400

    carinfo

--- Final/gen/com/example/freeuni/final_project/R.java
+++ Final/gen/com/example/freeuni/final_project/R.java
@@ -71,13 +71,18 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int descriprion1=0x7f050008;
+        public static final int descriprion2=0x7f050009;
+        public static final int descriprion3=0x7f05000a;
+        public static final int descriprion4=0x7f05000b;
+        public static final int descriprion5=0x7f05000c;
         public static final int hello_world=0x7f050002;
         public static final int join_friend=0x7f050004;
         public static final int one=0x7f050006;
         public static final int start_new=0x7f050003;
         public static final int submit=0x7f050005;
         public static final int two=0x7f050007;
-        public static final int type_friends_id=0x7f050008;
+        public static final int type_friends_id=0x7f05000d;
     }
     public static final class style {
         /** 
